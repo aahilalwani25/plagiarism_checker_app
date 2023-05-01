cba5a9fb5c3c37fdabc1b7e68ac6be2c057058f3
Aahil Alwani <alwani.aahil25@gmail.com>
2023-05-01 08:09:37 +0500

    initial commit

---
+++ lib/Views/onboard_screen.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/src/widgets/placeholder.dart';
+
+class OnboardScreen extends StatefulWidget {
+  const OnboardScreen({super.key});
+
+  @override
+  State<OnboardScreen> createState() => _OnboardScreenState();
+}
+
+class _OnboardScreenState extends State<OnboardScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          children: [
+            
+          ],
+        ),
+      ),
+    );
+  }
+}
