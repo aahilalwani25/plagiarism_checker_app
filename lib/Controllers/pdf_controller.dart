281f21d651ae92697a84fcdfcaa1e5bdc9fb9e41
Aahil Alwani <alwani.aahil25@gmail.com>
2023-06-24 00:29:46 +0500

    iniital commit

--- lib/Controllers/pdf_controller.dart
+++ lib/Controllers/pdf_controller.dart
@@ -1,32 +1,40 @@
+import 'package:flutter/services.dart';
 import 'package:open_file/open_file.dart';
+import 'package:pdf/pdf.dart';
 import 'package:plagiarism_checker_app/Models/content.dart';
 import 'dart:io';
 import 'package:path_provider/path_provider.dart';
 import 'package:pdf/widgets.dart';
 
-
-class PdfController{
-
+class PdfController {
   Content content;
   PdfController({required this.content});
 
-  Future<void> openFile() async{
-    final pdfFile= await _generatePdf();
+  Future<void> openFile() async {
+    final pdfFile = await _generatePdf();
     OpenFile.open(pdfFile.path);
   }
 
   Future<File> _generatePdf() async {
-    Document pdf = Document();
+    Document pdf = Document(
+        theme: ThemeData.withFont(
+          base: Font.ttf(await rootBundle.load("assets/OpenSans-Regular.ttf")),
+          bold: Font.ttf(await rootBundle.load("assets/OpenSans-Bold.ttf")),
+          italic: Font.ttf(await rootBundle.load("assets/OpenSans-Italic.ttf")),
+          boldItalic: Font.ttf(await rootBundle.load("assets/OpenSans-BoldItalic.ttf")),
+    ));
 
     pdf.addPage(MultiPage(
+        pageFormat: PdfPageFormat.a4,
         footer: (context) => Column(children: [
               Divider(),
-              Center(child: Text('Copyright (c) Aahil Alwani')),
+              Center(
+                  child: Text('Copyright (c) Aahil Alwani')),
               Positioned(right: 50, child: Text('Page 1 of 1'))
             ]),
         build: (context) => [
               _buildBarcode(),
-              Header(text: "PLAGIARISM CHECKER"),
+              Header(title: "PLAGIARISM CHECKER"),
               Text("Developed by Aahil Alwani"),
               Divider(),
               _buildTable(content),
@@ -53,13 +61,15 @@ class PdfController{
   }
 
   Widget _buildTable(Content content) {
-    final data = content.plagiarised!['values'].map((words) {
-      return [words.toString()];
-    });
-    return Table.fromTextArray(
+    final data = content.plagiarised!['values'] as List<dynamic>;
+
+    final updatedData = data.map((words) {
+      return [words];
+    }).toList();
+    return TableHelper.fromTextArray(
         headers: ['Website', 'List of words'],
-        headerStyle: TextStyle(fontWeight: FontWeight.bold),
-        data: data,
+        //headerStyle: TextStyle(fontWeight: FontWeight.bold),
+        data: updatedData,
         cellHeight: 30,
         cellAlignments: {
           0: Alignment.center,
--- lib/Views/Students/student_home_page.dart
+++ lib/Views/Students/student_home_page.dart
@@ -1,5 +1,6 @@
 import 'package:auto_size_text/auto_size_text.dart';
 import 'package:flutter/material.dart';
+import 'package:plagiarism_checker_app/Controllers/pdf_controller.dart';
 import 'package:provider/provider.dart';
 import '../../Controllers/plagiarism_controller.dart';
 import '../../Models/content.dart';
@@ -16,17 +17,16 @@ class _StudentHomePageState extends State<StudentHomePage> {
   final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
   final CustomTextBox _customTextBox = CustomTextBox();
 
-  Widget createPlagiarisedWords(dynamic content){
-
+  Widget createPlagiarisedWords(dynamic content) {
     return SizedBox(
-      height:  MediaQuery.of(context).size.height * 0.3,
+      height: MediaQuery.of(context).size.height * 0.3,
       child: ListView.builder(
-        itemCount: content.length,
-        itemBuilder: (itemBuilder,index){
-          return ListTile(
-            title: Text(content[index].toString()),
-          );
-      }),
+          itemCount: content.length,
+          itemBuilder: (itemBuilder, index) {
+            return ListTile(
+              title: Text(content[index].toString()),
+            );
+          }),
     );
   }
 
@@ -36,7 +36,7 @@ class _StudentHomePageState extends State<StudentHomePage> {
     //Content content = Provider.of<Content>(context, listen: false);
 
     return Scaffold(
-        
+
         // appBar: AppBar(
         //   backgroundColor: Colors.green,
         //   title: const AutoSizeText('Home'),
@@ -46,124 +46,137 @@ class _StudentHomePageState extends State<StudentHomePage> {
         //   ],
         // ),
         body: Consumer<Content>(builder: (builder, content, child) {
-          return Form(
-            key: _formKey,
-            child: Column(
-              children: [
-                const Center(
-                    child: AutoSizeText(
-                  "Check Plagiarised Content",
-                  textAlign: TextAlign.center,
-                  textDirection: TextDirection.ltr,
-                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 30),
-                )),
-                SizedBox(
-                  width: MediaQuery.of(context).size.width * 0.8,
-                  height: MediaQuery.of(context).size.height * 0.4,
-                  child: TextFormField(
-                    enabled: content.enabled,
-                    validator: (text) {
-                      if (text!.isEmpty) {
-                        return "Please enter the content";
-                      }
-                      return null;
-                    },
-                    initialValue: null,
-                    onChanged: (text) {
-                      //save text
-                      content.setContent(text);
-                    },
-                    maxLines: 20,
-                    maxLength: 200,
-                    decoration: _customTextBox.large_size(
-                        "Paste Text Here", Colors.grey[350]!, null),
-                  ),
-                ),
-                Padding(
-                  padding: EdgeInsets.only(
-                      top: MediaQuery.of(context).size.height * 0.01),
-                  child: SizedBox(
-                    width: MediaQuery.of(context).size.width * 0.8,
-                    height: MediaQuery.of(context).size.height * 0.05,
-                    child: AbsorbPointer(
-                      absorbing: content.enabled ? false : true,
-                      child: ActionChip(
-                        onPressed: () async {
-                          if (_formKey.currentState!.validate()) {
-                            content.setEnabled(false);
-                            PlagiarismController pc = PlagiarismController(text: content.content!);
-                            final Future<dynamic> plag= await pc.start_check(); 
-                            content.setPlagiarised(plag);
-                            content.setWordsCount(content.plagiarised!['values'].length);
-                            content.setEnabled(true);
-                          }
-                        },
-                        backgroundColor:
-                            content.enabled ? Colors.green : Colors.green[300],
-                        label: Center(
-                            child: content.enabled
-                                ? const Text(
-                                    "Submit",
-                                  )
-                                : const CircularProgressIndicator(
-                                    color: Colors.white,
-                                    strokeWidth: 2,
-                                  )),
-                      ),
-                    ),
-                  ),
-                ),
-
-                content.content==null
-                ?(const Text("Paste the content you want to plagiarise"))
-                : content.enabled==false
-                ? const Text("Please wait for detecting plagiarism")
-                :content.plagiarised != null
-                    ? Padding(
-                        padding: EdgeInsets.only(
-                            top: MediaQuery.of(context).size.height * 0.01),
-                        child: /*AutoSizeText(content.plagiarised!.toString()),*/ getList(content),
-                      )
-                    :  const Text("No Plagiarism"),
-                
-              ],
+      return Form(
+        key: _formKey,
+        child: Column(
+          children: [
+            const Center(
+                child: AutoSizeText(
+              "Check Plagiarised Content",
+              textAlign: TextAlign.center,
+              textDirection: TextDirection.ltr,
+              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 30),
+            )),
+            SizedBox(
+              width: MediaQuery.of(context).size.width * 0.8,
+              height: MediaQuery.of(context).size.height * 0.4,
+              child: TextFormField(
+                enabled: content.enabled,
+                validator: (text) {
+                  if (text!.isEmpty) {
+                    return "Please enter the content";
+                  }
+                  return null;
+                },
+                initialValue: null,
+                onChanged: (text) {
+                  //save text
+                  content.setContent(text);
+                },
+                maxLines: 20,
+                maxLength: 200,
+                decoration: _customTextBox.large_size(
+                    "Paste Text Here", Colors.grey[350]!, null),
+              ),
             ),
-          );
-        }));
+            Padding(
+              padding: EdgeInsets.only(
+                  top: MediaQuery.of(context).size.height * 0.01),
+              child: SizedBox(
+                width: MediaQuery.of(context).size.width * 0.8,
+                height: MediaQuery.of(context).size.height * 0.05,
+                child: AbsorbPointer(
+                  absorbing: content.enabled ? false : true,
+                  child: ActionChip(
+                    onPressed: () async {
+                      if (_formKey.currentState!.validate()) {
+                        content.setEnabled(false);
+                        PlagiarismController pc =
+                            PlagiarismController(text: content.content!);
+                        final plag = await pc.start_check();
+                        content.setPlagiarised(plag);
+                        content.setWordsCount(
+                            content.plagiarised!['values'].length);
+                        content.setEnabled(true);
+                      }
+                    },
+                    backgroundColor:
+                        content.enabled ? Colors.green : Colors.green[300],
+                    label: Center(
+                        child: content.enabled
+                            ? const Text(
+                                "Submit",
+                              )
+                            : const CircularProgressIndicator(
+                                color: Colors.white,
+                                strokeWidth: 2,
+                              )),
+                  ),
+                ),
+              ),
+            ),
+
+            Divider(),
+            content.content == null
+                ? (const Text("Paste the content you want to plagiarise"))
+                : content.enabled == false
+                    ? const Text("Please wait for detecting plagiarism")
+                    : content.plagiarised != null
+                        ? SizedBox(
+                            child: ActionChip(
+                              padding: EdgeInsets.all(8),
+                              backgroundColor: Colors.red,
+                              onPressed: ()async{
+                                PdfController pdfController= PdfController(content: content);
+                                await pdfController.openFile();
+                              },
+                                label: const Row(
+                                  mainAxisAlignment: MainAxisAlignment.center,
+                                  children: [
+                                    Text("Download Report"),
+                                    Icon(Icons.download, )
+                                  ],
+                                )),
+                          )
+                        : const Text("No Plagiarism"),
+          ],
+        ),
+      );
+    }));
   }
 
-  Widget getList(Content content){
+  Widget getList(Content content) {
     return AnimatedContainer(
       duration: const Duration(seconds: 1),
-      height: MediaQuery.of(context).size.height*0.2,
+      height: MediaQuery.of(context).size.height * 0.2,
       width: MediaQuery.of(context).size.width * 0.8,
       child: Column(
         children: [
           Container(
-            decoration: BoxDecoration(
-              color: Colors.red[400],
-              borderRadius: const BorderRadius.only(topLeft:Radius.circular(20), topRight: Radius.circular(20)),
-            ),
-            child: Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: AutoSizeText(content.plagiarised!['url']),
-            )
-          ),
-
+              decoration: BoxDecoration(
+                color: Colors.red[400],
+                borderRadius: const BorderRadius.only(
+                    topLeft: Radius.circular(20),
+                    topRight: Radius.circular(20)),
+              ),
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: AutoSizeText(content.plagiarised!['url']),
+              )),
           SizedBox(
-            height: MediaQuery.of(context).size.height*0.2,
+            height: MediaQuery.of(context).size.height * 0.2,
             child: ListView.builder(
-              itemCount: content.plagiarised!['values'].length,
-              itemBuilder: (itemBuilder,index){
-                return ListTile(
-                  shape: Border.all(style: BorderStyle.solid),
-                  style: ListTileStyle.list,
-                  tileColor: Colors.blue,
-                  title: AutoSizeText(
+                itemCount: content.plagiarised!['values'].length,
+                itemBuilder: (itemBuilder, index) {
+                  return ListTile(
+                    shape: Border.all(style: BorderStyle.solid),
+                    style: ListTileStyle.list,
+                    tileColor: Colors.blue,
+                    title: AutoSizeText(
                         content.plagiarised!['values'][index].toString()),
-                  //subtitle: AutoSizeText(content.plagiarised!['values'][index].toString()),
-                );
-            }),
+                    //subtitle: AutoSizeText(content.plagiarised!['values'][index].toString()),
+                  );
+                }),
           )
         ],
       ),
